710546a5595d56b2816514427bbc3a11118c7e83
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-24 11:26:38 +0600

    relationship establised between coures model and categry model

--- queries/courses.js
+++ queries/courses.js
@@ -1,7 +1,8 @@
+import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
 
 export async function getCourses() 
 {
-    const courses = await Course.find({})
+    const courses = await Course.find({}).populate({path:"category", model:Category })
     return courses;
 }
