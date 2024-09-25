bda8bece16ee725b50acab31e459d821465e145b
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-25 20:32:40 +0600

    Rating component added in Testimonials.jsx

--- app/(main)/courses/[id]/_components/Testimonials.jsx
+++ app/(main)/courses/[id]/_components/Testimonials.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import { SectionTitle } from "@/components/section-title";
+import StarRaing from "@/components/star-rating";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/components/ui/carousel";
 
-export default function Testimonials({courses}) {
+export default function Testimonials({testimonials}) {
   return (
     <section className="pb-8 md:pb-12 lg:pb-24">
     <div className="container">
@@ -22,9 +23,9 @@ export default function Testimonials({courses}) {
         <CarouselPrevious />
         <CarouselNext />
         <CarouselContent className="py-4">
-          {courses.map((course) => (
+          {testimonials && testimonials.map((testimonail) => (
             <CarouselItem
-              key={course.id}
+              key={testimonail.id}
               className="md:basis-1/2 lg:basis-1/3"
             >
               <div className="sm:break-inside-avoid">
@@ -32,65 +33,22 @@ export default function Testimonials({courses}) {
                   <div className="flex items-center gap-4">
                     <img
                       alt=""
-                      src="https://i.pravatar.cc/56"
+                      src={testimonail?.user?.profile_picture}
                       width="56"
                       height="56"
                       className="size-14 rounded-full object-cover"
                     />
                     <div>
                       <p className="mt-0.5 text-lg font-medium text-gray-900">
-                        John Doe
+                      {testimonail?.user?.first_name} {" "} {testimonail?.user?.last_name} 
                       </p>
                       <div className="flex justify-center gap-0.5 text-yellow-600">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
+                     <StarRaing rating={testimonail?.rating}/>
                       </div>
                     </div>
                   </div>
                   <p className="mt-4 text-gray-700">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing
-                    elit. Culpa sit rerum incidunt, a consequuntur
-                    recusandae ab saepe illo est quia obcaecati neque
-                    quibusdam eius accusamus error officiis atque voluptates
-                    magnam!
+                  {testimonail?.content}
                   </p>
                 </blockquote>
               </div>
