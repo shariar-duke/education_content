6b24a27f06238fe99e21a08dd7b42343d4ca4aca
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-25 12:56:07 +0600

    testimonial mode modified

--- model/testimonial-model.js
+++ model/testimonial-model.js
@@ -1,24 +1,24 @@
 import mongoose, { Schema } from "mongoose";
 
 const testimonialSchema = new Schema({
-    content: {
-        required: true,
-        type: String,
-    },
-    user: {
-        required: true,
-        type: String,
-    },
-    courseId: {
-        required: true,
-        type: String,
-    },
-    rating: {
-        required: true,
-        type: Number,
-    },
+  content: {
+    required: true,
+    type: String,
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: "User",
+  },
+  courseId: {
+    required: true,
+    type: String,
+  },
+  rating: {
+    required: true,
+    type: Number,
+  },
 });
 
 export const Testimonial =
-    mongoose.models.Testimonial ??
-    mongoose.model("Testimonial", testimonialSchema);
+  mongoose.models.Testimonial ??
+  mongoose.model("Testimonial", testimonialSchema);
