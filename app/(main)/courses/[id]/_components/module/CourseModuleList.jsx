b019ab33b4b4967721073396165648520bb10bc4
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-27 00:46:38 +0600

    Lessons changed to Hourse

--- app/(main)/courses/[id]/_components/module/CourseModuleList.jsx
+++ app/(main)/courses/[id]/_components/module/CourseModuleList.jsx
@@ -16,7 +16,7 @@ export default function CourseModuleList({ module }) {
           <div class="flex gap-x-5 items-center flex-wrap mt-4 mb-6 text-gray-600 text-sm">
             <span className="flex items-center gap-1.5">
               <Video className="w-4 h-4" />
-              {(module?.duration / 60).toPrecision(2)} Lessons
+              {(module?.duration / 60).toPrecision(2)} Hours
             </span>
             <span className="flex items-center gap-1.5">
               <NotepadText className="w-4 h-4" />
