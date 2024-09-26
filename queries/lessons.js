786e5284a4a1c0c4bf0a1dcbc6185a6744c9ff3c
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-27 00:44:01 +0600

    CourseLessonList component done

--- queries/lessons.js
+++ queries/lessons.js
@@ -1,5 +1,5 @@
 import { replaceMongoIdInObject } from "@/lib/convertData";
-import { Lesson } from "@/model/lesson.model";
+import { Lesson } from "@/model/lesson-model";
 
 export async function getLesson(lessonId) {
     const lesson = await Lesson.findById(lessonId).lean();
