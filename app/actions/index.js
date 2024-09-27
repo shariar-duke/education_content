acbb05fc6c0e927b2311d2b324f97f616b5b4b94
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-27 11:35:41 +0600

    server acation created for login

---
+++ app/actions/index.js
@@ -0,0 +1,17 @@
+"use server";
+
+const { signIn } = require("@/auth");
+
+export async function credentailLogin(formData) {
+  try {
+    const response = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
