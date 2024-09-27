63889459db99af4d01e1e2f9466a164893dfdda2
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-27 12:06:41 +0600

    login complemteed with server action

--- app/login/_components/login-form.jsx
+++ app/login/_components/login-form.jsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 
+import { credentailLogin } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,8 +12,26 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
+import { useRouter } from "next/navigation";
 export function LoginForm() {
+  const router = useRouter();
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await credentailLogin(formData);
+
+      if (!!response.error) {
+        console.error(response.error);
+        setError(response.error);
+      } else {
+        router.push("/courses");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
   return (
     <Card className="mx-auto max-w-sm w-full">
       <CardHeader>
@@ -21,29 +41,32 @@ export function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              {/* <Link href="#" className="ml-auto inline-block text-sm underline">
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                {/* <Link href="#" className="ml-auto inline-block text-sm underline">
                 Forgot your password?
               </Link> */}
+              </div>
+              <Input id="password" type="password" name="password" required />
             </div>
-            <Input id="password" type="password" required />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
           </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </div>
+        </form>
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
           <Link href="register" className="underline">
