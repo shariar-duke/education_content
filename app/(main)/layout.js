bbdfe7fff9a4ca626a9499548404deb6f780e3ba
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-27 14:42:25 +0600

    mistake correction in layout.js

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -23,7 +23,7 @@ const MainLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col">
       <header className="z-40 bg-background/60 backdrop-blur-md fixed top-0 left-0 right-0 border-b ">
-        <SessionProvider>
+      <SessionProvider>
           <div className="container flex h-20 items-center justify-between py-6 ">
             <MainNav items={navLinks} />
           </div>
