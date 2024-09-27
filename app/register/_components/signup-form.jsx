4f60b7b99359a1b397e50d529d23a9649a60e2cd
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-27 10:09:13 +0600

    registraion api called form sign-up form

--- app/register/_components/signup-form.jsx
+++ app/register/_components/signup-form.jsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-
+"use client"
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,8 +9,50 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export function SignupForm() {
+export function SignupForm({ role }) {
+
+  const router = useRouter()
+  
+  async function handleSubmit(event) {
+     event.preventDefault();
+
+     try{
+      const formData = new FormData(event.currentTarget);
+      const firstName = formData.get("first-name")
+      const lastName = formData.get("last-name")
+      const email = formData.get("email")
+      const password = formData.get("password")
+
+      const userRole = (role === "student" || "instructor") ? role : "student"
+
+      // now call the register 
+
+     const response = await fetch("/api/register", {
+        method:"POST",
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+          firstName:firstName,
+          lastName:lastName,
+          email:email,
+          password:password,
+          userRole:userRole
+
+        })
+      })
+
+      response.status === 201 && router.push("/login")
+
+     }catch(e) 
+     {
+      console.log(e.message)
+     }
+  }
+   
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -21,38 +62,42 @@ export function SignupForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" name="first-name" placeholder="Max" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" name="last-name" placeholder="Robinson" required />
+              </div>
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+              />
             </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" name="password" type="password" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="confirmPassword">Confirm Password</Label>
+              <Input id="confirmPassword" name="confirmPassword"  type="password" />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="confirmPassword">Confirm Password</Label>
-            <Input id="confirmPassword" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
-        </div>
+        </form>
+
         <div className="mt-4 text-center text-sm">
           Already have an account?{" "}
           <Link href="/login" className="underline">
