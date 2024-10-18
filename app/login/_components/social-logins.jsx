c92e9e1864526abcb7598e9273c0f0127f2e1b7c
Shariar Duke <shariarmahmud225@gmail.com>
2024-10-18 11:17:20 +0600

    socail login component created

---
+++ app/login/_components/social-logins.jsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+export default function SocialLogins() {
+  return (
+    <>
+      <div className="text-center text-md mt-3 text-gray-500">
+        or Signup with
+      </div>
+      <form>
+        <div className="flex justify-center gap-2">
+          <Button
+            className="mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+            type="submit"
+            name="action"
+            value="google"
+          >
+            <Image src="/google.png" alt="google" width={30} height={30} />
+            <span>Google</span>
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
