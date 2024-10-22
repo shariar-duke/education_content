bad229083674f0f8b5ad2b1f4ea5f61a633be4eb
Shariar Duke <shariarmahmud225@gmail.com>
2024-10-22 16:05:57 +0600

    enroll-course button created

---
+++ components/enroll-course.jsx
@@ -0,0 +1,23 @@
+import { Button, buttonVariants } from "@/components/ui/button"; // Check if this Button supports the used props
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+export const EnrollCourse = ({ asLink }) => {
+  return (
+    <>
+      {asLink ? (
+        <Button
+          variant="ghost"
+          type="submit"
+          className="text-xs text-sky-700 h-7 gap-1"
+        >
+          Enroll
+          <ArrowRight className="w-3" />{" "}
+        </Button>
+      ) : (
+        <Button type="submit" className={cn(buttonVariants({ size: "lg" }))}>
+          Enroll Now
+        </Button>
+      )}
+    </>
+  );
+};
