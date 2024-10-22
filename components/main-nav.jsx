e0e9e1d9e0339ca67bc0d19838c833ebd9339c58
Shariar Duke <shariarmahmud225@gmail.com>
2024-10-22 15:18:24 +0600

    package installed

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -9,121 +9,123 @@ import { Logo } from "./logo";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button, buttonVariants } from "./ui/button";
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
-
 import { signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 export function MainNav({ items, children }) {
-	const { data: session, status } = useSession(); 
-	console.log("The current session is", session)
-	const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const { data: session, status } = useSession();
+  console.log("The current session is", session);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [loginSession, setLoginSession] = useState(null);
+  console.log(session);
 
-	const [loginSession, setLoginSession] = useState(null);
+  useEffect(() => {
+    console.log("The current session is");
+    setLoginSession(session);
+  }, [session]);
 
-	useEffect(() => {
-		console.log("The current session is", );
-        setLoginSession(session);
-    }, [session]);
+  if (session?.error === "RefreshAccessTokenError") {
+    redirect("/login");
+  }
 
-
-	return (
-		<>
-			<div className="flex gap-6 lg:gap-10">
-				<Link href="/">
-					<Logo />
-				</Link>
-				{items?.length ? (
-					<nav className="hidden gap-6 lg:flex">
-						{items?.map((item, index) => (
-							<Link
-								key={index}
-								href={item.disabled ? "#" : item.href}
-								className={cn(
-									"flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
-								)}>
-								{item.title}
-							</Link>
-						))}
-					</nav>
-				) : null}
-
-				{showMobileMenu && items && (
-					<MobileNav items={items}>{children}</MobileNav>
-				)}
-			</div>
-			<nav className="flex items-center gap-3">
-			{!loginSession && (
-                    <div className="items-center gap-3 hidden lg:flex">
-                        <Link
-                            href="/login"
-                            className={cn(
-                                buttonVariants({ size: "sm" }),
-                                "px-4"
-                            )}
-                        >
-                            Login
-                        </Link>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <Button variant="outline" size="sm">
-                                    Register
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent
-                                align="end"
-                                className="w-56 mt-4"
-                            >
-                                <DropdownMenuItem className="cursor-pointer">
-                                    <Link href="/register/student">
-                                        Student
-                                    </Link>
-                                </DropdownMenuItem>
-                                <DropdownMenuItem className="cursor-pointer">
-                                    <Link href="/register/instructor">
-                                        Instructor
-                                    </Link>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </div>
+  return (
+    <>
+      <div className="flex gap-6 lg:gap-10">
+        <Link href="/">
+          <Logo />
+        </Link>
+        {items?.length ? (
+          <nav className="hidden gap-6 lg:flex">
+            {items?.map((item, index) => (
+              <Link
+                key={index}
+                href={item.disabled ? "#" : item.href}
+                className={cn(
+                  "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
                 )}
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<div className="cursor-pointer">
-							<Avatar>
-								<AvatarImage
-									src="https://github.com/shadcn.png"
-									alt="@shadcn"
-								/>
-								<AvatarFallback>CN</AvatarFallback>
-							</Avatar>
-						</div>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end" className="w-56 mt-4">
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="account">Profile</Link>
-						</DropdownMenuItem>
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="account/enrolled-courses">My Courses</Link>
-						</DropdownMenuItem>
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="">Testimonials & Certificates</Link>
-						</DropdownMenuItem>
-						<DropdownMenuItem className="cursor-pointer" asChild>
-						<Link href="#" onClick={() => {signOut()}}>Logout</Link>
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-				<button
-					className="flex items-center space-x-2 lg:hidden"
-					onClick={() => setShowMobileMenu(!showMobileMenu)}>
-					{showMobileMenu ? <X /> : <Menu />}
-				</button>
-			</nav>
-		</>
-	);
+              >
+                {item.title}
+              </Link>
+            ))}
+          </nav>
+        ) : null}
+
+        {showMobileMenu && items && (
+          <MobileNav items={items}>{children}</MobileNav>
+        )}
+      </div>
+      <nav className="flex items-center gap-3">
+        {!loginSession && (
+          <div className="items-center gap-3 hidden lg:flex">
+            <Link
+              href="/login"
+              className={cn(buttonVariants({ size: "sm" }), "px-4")}
+            >
+              Login
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="sm">
+                  Register
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-56 mt-4">
+                <DropdownMenuItem className="cursor-pointer">
+                  <Link href="/register/student">Student</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">
+                  <Link href="/register/instructor">Instructor</Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        )}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="cursor-pointer">
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-56 mt-4">
+            <DropdownMenuItem className="cursor-pointer" asChild>
+              <Link href="account">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" asChild>
+              <Link href="account/enrolled-courses">My Courses</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" asChild>
+              <Link href="">Testimonials & Certificates</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" asChild>
+              <Link
+                href="#"
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Logout
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <button
+          className="flex items-center space-x-2 lg:hidden"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        >
+          {showMobileMenu ? <X /> : <Menu />}
+        </button>
+      </nav>
+    </>
+  );
 }
