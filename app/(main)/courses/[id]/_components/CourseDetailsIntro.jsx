57fa8f366b8a4b356b6b1080c6dd831f017c169f
Shariar Duke <shariarmahmud225@gmail.com>
2024-10-22 16:06:43 +0600

    button added in CourseDetailsIntro.jsx

--- app/(main)/courses/[id]/_components/CourseDetailsIntro.jsx
+++ app/(main)/courses/[id]/_components/CourseDetailsIntro.jsx
@@ -1,18 +1,16 @@
+import { EnrollCourse } from "@/components/enroll-course";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-
-export default function CourseDetailsIntro({title, subtitle, thumbnail}) {
+export default function CourseDetailsIntro({ title, subtitle, thumbnail }) {
   return (
     <div className="overflow-x-hidden  grainy">
       <section className="pt-12  sm:pt-16">
         <div className="container">
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="max-w-2xl mx-auto text-center">
-              <h1 className="px-6 text-lg text-gray-600 font-inter">
-               {title}
-              </h1>
+              <h1 className="px-6 text-lg text-gray-600 font-inter">{title}</h1>
               <p className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:leading-tight sm:text-5xl lg:text-6xl lg:leading-tight font-pj">
                 <span className="relative inline-flex sm:inline">
                   <span className="bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-lg filter opacity-30 w-full h-full absolute inset-0"></span>
@@ -21,9 +19,8 @@ export default function CourseDetailsIntro({title, subtitle, thumbnail}) {
               </p>
 
               <div className="mt-6 flex items-center justify-center flex-wrap gap-3">
-                <Link href="" className={cn(buttonVariants({ size: "lg" }))}>
-                  Enroll Now
-                </Link>
+                <EnrollCourse />
+
                 <Link
                   href=""
                   className={cn(
